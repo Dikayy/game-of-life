737f21e30f06d7df2f04a8c88f85738bc62fe193
Noah Protte <nprotte@gmail.com>
2022-12-08 00:02:16 +0100

    Added functionality to click cells

--- cell.py
+++ cell.py
@@ -3,12 +3,25 @@ import pygame
 class Cell:
     alive = False
     WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
 
     def __init__(self, WIN, xPos, yPos, size):
-        #self.WIN = WIN
+        self.WIN = WIN
         self.xPos = xPos
         self.yPos = yPos
         self.size = size
 
-        rect = pygame.Rect(self.xPos, self.yPos, self.size, self.size)
-        pygame.draw.rect(WIN, self.WHITE, rect)
+        self.rect = pygame.Rect(self.xPos, self.yPos, self.size, self.size)
+        pygame.draw.rect(self.WIN, self.WHITE, self.rect)
+
+    def is_in_pos(self, pos):
+        return self.rect.collidepoint(pos)
+    
+    def set_alive(self, alive):
+        if alive:
+            pygame.draw.rect(self.WIN, self.BLACK, self.rect)
+        else:
+            pygame.draw.rect(self.WIN, self.WHITE, self.rect)
+        
+        self.alive = alive
+    
--- game.py
+++ game.py
@@ -4,16 +4,15 @@ from cell import Cell
 class Game:
     WIDTH, HEIGHT = 900, 700
     FPS = 60
-    #grid = []
 
     def __init__(self):
         self.WIN = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         pygame.display.set_caption("Conway's Game Of Life")
 
         self.WIN.fill((158, 158, 158))
-        self.init_grid(30)
+        self.grid = self.init_grid(30)
         self.main_loop()
-    
+
     def main_loop(self):
         clock = pygame.time.Clock()
         run = True
@@ -23,10 +22,19 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    self.click_cell(pygame.mouse.get_pos())
             
             pygame.display.update()
         
         pygame.quit()
+
+    def click_cell(self, pos):
+        for column in self.grid:
+            for cell in column:
+                if cell.is_in_pos(pos):
+                    alive = cell.alive
+                    cell.set_alive(not alive)
 
     def init_grid(self, size):
         grid = []
@@ -40,4 +48,6 @@ class Game:
                 xPos = border + i * cellSize + i * border
                 yPos = border + j * cellSize + j * border
                 column.append(Cell(self.WIN, xPos, yPos, cellSize))
+        
+        return grid
     
